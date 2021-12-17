67c158095d84eb54e97c4584e29a2ff53775418f
Pranav Kale <pranavkale021998@gmail.com>
2021-12-17 09:55:37 +0530

    '17-12-2021'

--- src/ScreenShare.java
+++ src/ScreenShare.java
@@ -1,13 +1,21 @@
 import java.awt.AWTException;
+import java.awt.Button;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
+import java.awt.GridLayout;
+import java.awt.Label;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Robot;
+import java.awt.TextField;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -15,6 +23,7 @@ import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Random;
 import java.util.Scanner;
@@ -22,48 +31,158 @@ import java.util.StringTokenizer;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageInputStream;
-import javax.imageio.stream.MemoryCacheImageInputStream;
 import javax.swing.JFrame;
 
-class RenderThread extends Thread {
-	ArrayList<BufferedImage> renderBuffer = new ArrayList<BufferedImage>();
-	ImagePanel panel;
-
-	public RenderThread( ArrayList<BufferedImage> renderBuffer, ImagePanel p  ) {
-		this.renderBuffer = renderBuffer; 
-		this.panel = p;
-		this.start();
-	}
-
-	public void run (){
-		for( int i=0 ; i<30 ; i++ ) {
-			System.out.println( "render" );
-			panel.repaint(); 
-			panel.setImg( renderBuffer.get( i ) );
-
-			try{ 
-				Thread.sleep( 130 );
-			}
-			catch( Exception e ){ 
-
-			}
-		}
-	}
-}
-
 public class ScreenShare {
+	ServerSocket  server;
+	Socket socket;	
+	boolean flag;
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Screen Sharer");
-		System.out.println("To set up server, type server [port]");
-		System.out.println("To Set up client, type client [server-addr] [port]");
-
 		new ScreenShare().interactive();
+		// new ScreenShare().loadGui();
 	}
-	
-	// move Mouse per five second while in client mode
-	private boolean mouseMove;
+
+	private void loadGui() {
+		Font large = new Font("Times New Roman", Font.PLAIN, 22 );
+
+		JFrame f = new JFrame();
+		f.setResizable(false);
+		f.setVisible(true);
+		f.setSize( 400, 600 ); 	
+		f.setLayout( null ); 
+		f.getContentPane().setBackground( Color.GRAY );
+
+		// Join a meeting menu 
+		Label h1 = new Label("Join a Meeting"); 
+		h1.setFont( large ); 
+		h1.setBounds( 30, 30, 200, 50 ); 
+		h1.setForeground( Color.WHITE );
+		f.add( h1 );
+
+		Label l1 = new Label("Host Ip");
+		l1.setFont( large );
+		l1.setBounds( 80, 100, 300, 50 );
+		l1.setForeground( Color.WHITE );
+		f.add( l1 );
+
+		TextField tf = new TextField(); 
+		tf.setBounds( 80, 150, 200, 50 );
+		tf.setFont( large );
+		f.add( tf );
+
+		Label l2 = new Label("Host Port");
+		l2.setFont( large );
+		l2.setBounds( 80, 200, 300, 50 );
+		l2.setForeground( Color.WHITE );
+		f.add( l2 );
+
+		TextField tf2 = new TextField(); 
+		tf2.setBounds( 80, 250, 200, 50 );
+		tf2.setFont( large );
+		f.add( tf2 );
+
+		Button btn = new Button("Join");
+		btn.setBounds( 80, 320, 200, 50 );
+		btn.setFont( large ); 
+		btn.addActionListener( new ActionListener( ) {
+			public void actionPerformed( ActionEvent ae ) {
+				// checking if the ip is valid
+				String s = tf.getText();
+				String s2 = tf2.getText();
+
+				// try {
+				intepretCommand( "client " + s + " " + s2 );
+					// client( s , Integer.parseInt( s2 ) );
+
+				// }
+				// catch( IOException ioe ) {
+
+				// 	ioe.printStackTrace();
+				// }
+				
+
+				// if( s.length() != 0 ) {
+				// 	// dividing the ip using '.'
+
+				// 	int count=0; 
+				// 	for( int i=0; i<s.length(); i++ ) {
+				// 		if( s.charAt(i) == '.' ) {
+				// 			count++;
+				// 		}
+				// 	}
+
+				// 	if( count != 3 ) {
+				// 		// TODO:
+				// 		// showing dialog - Not Valid
+				// 	}
+				// 	else {
+				// 		String s2 = tf2.getText();
+				// 		if( s2.length() != 0 ) {
+				// 			// checking if there is a host at the endpoint 
+				// 			try {
+				// 				// swtiching screens, running the client application 
+				// 				System.out.println("Connecting to meeting at " + s + ":"+s2 );
+
+				// 				client( s , Integer.parseInt( s2 ) );
+				// 			}
+				// 			catch( Exception e ) {
+				// 				// TODO:
+				// 				// show dialog - Invalid meeting endpoint 
+				// 				System.out.println("Invalid meeting endpoint ");
+				// 			}
+				// 		}
+				// 	}
+				// }
+			}
+		});
+		f.add( btn );
+
+
+		// Host a meeting menu
+		Button h2 = new Button("Host a Meeting"); 
+		h2.setBounds( 30, 440, 200, 50 );
+		h2.setFont( large ); 
+		h2.addActionListener( new ActionListener() {
+			public void actionPerformed( ActionEvent ae ) {
+				intepretCommand("server 400" );
+				// int startPortRange=1024;
+				// int stopPortRange=65365;       
+			
+				// for(int i=startPortRange; i <=stopPortRange; i++)
+				// {
+				// 	ServerSocket ser=null;
+
+				// 	try
+				// 	{
+				// 		ser = new ServerSocket(i);
+				// 		final int port = i;
+				// 		ser.close();
+
+				// 		// port avaialable, Hosting the meeting... 
+				// 		System.out.println("Hosting the meeting on port " + i );
+
+				// 		server( port );
+						
+				// 		// Thread t1 = new Thread() {
+				// 		// 	public void run() {
+				// 		// 		server( port );
+				// 		// 	}
+				// 		// };
+
+				// 		// t1.start();
+
+				// 		return;
+				// 	}
+				// 	catch (Exception e)
+				// 	{	
+				// 		e.printStackTrace(); 
+				// 	}
+				// }
+			}
+		});
+		f.add( h2 );
+	}	
 
 	private void interactive() {
 		Scanner s = new Scanner(System.in);
@@ -74,22 +193,13 @@ public class ScreenShare {
 				if (s.hasNext()) {
 					intepretCommand(s.nextLine());
 				}
-
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			} catch (Exception e) {
-				e.printStackTrace();
+			} 
+			catch (Exception e) {
+				
 			}
-
 		}	
-
 	}
-
-	/**
-	 * Intepret a given command
-	 * 
-	 * @param cmd
-	 */
+	
 	private void intepretCommand(String cmd) {
 		StringTokenizer tokenizer = new StringTokenizer(cmd);
 		String commandToken = tokenizer.nextToken();
@@ -100,16 +210,15 @@ public class ScreenShare {
 		else if (commandToken.equals("client")) {
 			String serverAddr = tokenizer.nextToken();
 			String port = tokenizer.nextToken();
-			client(serverAddr, Integer.parseInt(port));
+			try {
+				client(serverAddr, Integer.parseInt(port));
+			}
+			catch( Exception e ){
+				e.printStackTrace();
+			}
 		} 
 		else if (commandToken.equals("close")){
 			close();
-		} 
-		else if (commandToken.equals("mousemove")){
-			this.mouseMove= true;
-		} 
-		else if (commandToken.equals("nomousemove")){
-			this.mouseMove= false;
 		} 
 		else {
 			System.out.println("Unrecognized Command");
@@ -117,108 +226,73 @@ public class ScreenShare {
 
 	}
 
-	private void client(String serverAddr, int port) {
+	private void client(String serverAddr, int port) throws IOException {
+		flag = true;
 		JFrame frame = new JFrame();
-		frame.setTitle( "Screen Share");
+		
 		ImagePanel panel = new ImagePanel();
 		ArrayList<BufferedImage> imageBuffer = new ArrayList<BufferedImage>();
 		ArrayList<BufferedImage> renderBuffer = new ArrayList<BufferedImage>();
 
+		frame.setTitle( "Virtual Classroom" );
 		frame.setResizable(true);
+		frame.setVisible(true);
+		frame.getContentPane().setPreferredSize( new Dimension( 1366, 768 ) );
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent event) {
+				flag = false;
+				frame.dispose();
+			}
+		});
+		frame.setLayout( null );
 		frame.add(panel);
 		frame.pack();
-		frame.setVisible(true);
 
-		try {
+		Dimension d = frame.getContentPane().getPreferredSize();
+		panel.setSize( (int) d.getWidth()  , (int) d.getHeight() );
 
+		// Label fps = new Label();
+		// fps.setBounds( (int) d.getWidth() - 100 , 50 , 40, 20 );
+		// frame.add(fps);
+
+		try 
+		{
 			long startTime = System.currentTimeMillis();
 			Random rand = new Random();
 
 			int frameCount = 0 ;
-			while(true) {
+			while(flag) {
 				socket = new Socket(serverAddr, port);
-				ObjectInputStream inputStream = new ObjectInputStream( socket.getInputStream());
+				ObjectInputStream inputStream = new ObjectInputStream( socket.getInputStream() );
 				
 				panel.repaint();
-				
+		
 				if( startTime > 1000 ){
-					
 					renderBuffer = ( ArrayList<BufferedImage> ) imageBuffer.clone();
+					
 					imageBuffer.clear();
 
-					new RenderThread( renderBuffer, panel );
-
+					new RenderThread( renderBuffer, panel, panel.getSize() );
 					startTime = System.currentTimeMillis();
-				}
-
-				// adding the current image into the buffer 
-				// if( imageBuffer.size() > 30 ) {
-				// 	renderBuffer = ( ArrayList<BufferedImage> )imageBuffer.clone();
-				// 	imageBuffer.clear();
-
-				// 	new RenderThread( renderBuffer, panel );
-					
-
-					// if( frameCount <= 30 ) {
-					// 	new RenderThread( imageBuffer, panel );
-					// }
-					// else {
-					// 	new RenderThread( renderBuffer, panel );
-					// 	renderBuffer.clear();
-					// }
-					
-					// renderBuffer.add( imageBuffer.get( frameCount ) );
-					// frameCount++;
-					
-					// panel.repaint(); 
-					// panel.setImg( imageBuffer.get( frameCount ) );
-					// // panel.setImg( ScreenShare.resize( imageBuffer.get( 0 ), frame.getSize().width - 50 , frame.getSize().height - 50 ) );
-					
-					// // imageBuffer.remove( frameCount <= 30 ? 30 - frameCount : frameCount - 30 );	
-					// // imageBuffer.remove( 0 );
-					// frameCount++;
-				// }
-				else {
-					System.out.println( "Buffer Size :" + imageBuffer.size() );
 				}
 
 				imageBuffer.add( ImageIO.read( inputStream ) );
 				socket.close();
-				
-				// socket = new Socket(serverAddr, port);
-				// ObjectInputStream inputStream = new ObjectInputStream( socket.getInputStream());
-				
-				// panel.repaint();
-				
-				// // adding the current image into the buffer 
-				// if( imageBuffer.size() > 200 ) {
-				// 	for( int i=0; i<= 200 ; i++ ) {
-				// 		try{ 
-				// 			Thread.sleep( 4 );
-				// 		}
-				// 		catch( Exception e ) {
-
-				// 		}
-				// 		panel.repaint();
-				// 		panel.setImg( ScreenShare.resize( imageBuffer.get( i ), frame.getSize().width - 50 , frame.getSize().height - 50 ) );
-				// 	}
-				// 	imageBuffer.clear();
-				// }
-				// else {
-				// 	System.out.println( "Buffer Size :" + imageBuffer.size() );
-				// }
-				// imageBuffer.add( ImageIO.read( inputStream ) );
-
-				// socket.close();
 			}
 		} 
-		catch (IOException e) {
+		catch( SocketException e ) {
 			e.printStackTrace();
+		}
+		catch (IOException e) {
+			frame.dispose();
+			throw e;
 		} 
+		catch( Exception e ) {
+			e.printStackTrace();
+		}
 	}
-
-	ServerSocket  server;
-	Socket socket;
 	
 	private void close(){
 		try {
@@ -228,6 +302,7 @@ public class ScreenShare {
 			e.printStackTrace();
 		}
 	}
+
 	private void server(int port) {
 		try {
 			server = new ServerSocket(port);
@@ -235,9 +310,10 @@ public class ScreenShare {
 
 			while(true){
 				try{
+					
 					socket = server.accept();
 					InetAddress addr = socket.getInetAddress();
-					System.out.println( "Received Connection From " + addr.getCanonicalHostName() + " at " + addr.getHostAddress() );
+					// System.out.println( "Received Connection From " + addr.getCanonicalHostName() + " at " + addr.getHostAddress() );
 					
 					ObjectOutputStream outstream = new ObjectOutputStream(socket.getOutputStream());
 					BufferedImage img;
@@ -251,28 +327,20 @@ public class ScreenShare {
 					ImageIO.write(img, "jpg", outstream);
 					socket.close();
 				} 
+				catch( SocketException se ) {
+					System.out.println("SE");
+					throw se;
+				}  
 				catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
-		}  
+		}
 		catch (AWTException e) {
 			e.printStackTrace();
 		} 
 		catch (IOException e1) {
 			e1.printStackTrace();
 		} 
-	}
-	
-	public static BufferedImage resize(BufferedImage img, int newW, int newH) { 
-	    Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_AREA_AVERAGING );
-		tmp.setAccelerationPriority( 1.0f );
-	    BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
-
-	    Graphics2D g2d = dimg.createGraphics();
-	    g2d.drawImage(tmp, 0, 0, null);
-	    g2d.dispose();
-
-	    return dimg;
-	}  
+	}	
 }
